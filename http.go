d25c34e3e3e3ab8f9fdf15118712326fad15370f
霍根 <15074685969@163.com>
2019-08-11 11:43:01 +0800

    1

--- http.go
+++ http.go
@@ -11,7 +11,8 @@ import (
 )
 
 func main() {
-	l, err := net.Listen("tcp", ":8080")
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	l, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Panic(err)
 	}
@@ -26,54 +27,48 @@ func main() {
 	}
 }
 
-func handleClientRequest(conn net.Conn) {
-	if conn == nil {
-		log.Println("no client")
+func handleClientRequest(client net.Conn) {
+	if client == nil {
 		return
 	}
-	defer conn.Close()
+	defer client.Close()
 
 	var b [1024]byte
-
-	n, err := conn.Read(b[:])
-
+	n, err := client.Read(b[:])
 	if err != nil {
 		log.Println(err)
 		return
 	}
 	var method, host, address string
 	fmt.Sscanf(string(b[:bytes.IndexByte(b[:], '\n')]), "%s%s", &method, &host)
-
-	hostPort, err := url.Parse(host)
-
+	hostPortURL, err := url.Parse(host)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	if hostPort.Opaque == "443" {
-		address = hostPort.Scheme + ":443"
-	} else {
-		if strings.Index(hostPort.Host, ":") == -1 {
-			address = hostPort.Host + "80"
+	if hostPortURL.Opaque == "443" { //https访问
+		address = hostPortURL.Scheme + ":443"
+	} else { //http访问
+		if strings.Index(hostPortURL.Host, ":") == -1 { //host不带端口， 默认80
+			address = hostPortURL.Host + ":80"
 		} else {
-			address = hostPort.Host
+			address = hostPortURL.Host
 		}
 	}
 
+	//获得了请求的host和port，就开始拨号吧
 	server, err := net.Dial("tcp", address)
-
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
 	if method == "CONNECT" {
-		fmt.Fprint(conn, "HTTP/1.1 200 Connection established\r\n\r\n")
+		fmt.Fprint(client, "HTTP/1.1 200 Connection established\r\n\r\n")
 	} else {
 		server.Write(b[:n])
 	}
-
-	go io.Copy(server, conn)
-	io.Copy(conn, server)
+	//进行转发
+	go io.Copy(server, client)
+	io.Copy(client, server)
 }
